c1e070b1df768ce11515548801bfaa3df94be963
Stefan Knipfer <knipfer@student.tugraz.at>
2026-07-24 14:18:58 +0200

    Spray can CI runner test

--- test/integration/spray_tool_test.dart
+++ test/integration/spray_tool_test.dart
@@ -63,7 +63,7 @@ void main() {
       await UIInteraction.createNewImage();
       await UIInteraction.selectTool(ToolData.SPRAY.name);
 
-      const radius = 50.0;
+      const radius = 20.0;
 
       (UIInteraction.getCurrentTool() as SprayTool).updateSprayRadius(radius);
 
@@ -94,7 +94,7 @@ void main() {
       await UIInteraction.createNewImage();
       await UIInteraction.selectTool(ToolData.SPRAY.name);
 
-      const radius = 50.0;
+      const radius = 20.0;
 
       (UIInteraction.getCurrentTool() as SprayTool).updateSprayRadius(radius);
 
